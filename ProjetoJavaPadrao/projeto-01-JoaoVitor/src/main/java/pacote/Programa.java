fb70527fd6accd40aee0a61e6610f055514b2f69
João Vitor <joao.vitor.rodrigues.tele@gmail.com>
2021-07-30 04:11:40 -0300

    Add files via upload

---
+++ ProjetoJavaPadrao/projeto-01-JoaoVitor/src/main/java/pacote/Programa.java
@@ -0,0 +1,15 @@
+package pacote;
+
+import static java.lang.System.out;
+
+/**
+ * Comentar sobre o Programa.
+ */
+public class Programa { //OMG, this is horrible, Bob is an  !!!
+  /**
+   * Comentar sobre o main.
+   */
+  public static void main(String[] args) {
+    out.println("Dale");
+  }
+}
